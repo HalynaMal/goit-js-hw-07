0374564dbbdf3357a95291164d4460d82551b857
HalynaMal <dguoit@ukr.net>
2023-12-25 22:07:14 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,14 @@
-const textInput = document.querySelector("#name-input");
+function updateName() {
+            // Отримуємо значення інпута та видаляємо пробіли по краях
+            let nameInput = document.getElementById("name-input").value.trim();
+
+            // Отримуємо елемент для виводу імені
+            let nameOutput = document.getElementById("name-output");
+
+            // Перевіряємо, чи ім'я не порожнє або не містить лише пробіли
+            if (nameInput !== "") {
+                nameOutput.textContent = nameInput;
+            } else {
+                nameOutput.textContent = "Anonymous";
+            }
+        }
