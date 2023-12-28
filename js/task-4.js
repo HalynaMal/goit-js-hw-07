5d60253a6ade805c7add6d9fc1279ca813358e57
Halyna <dguoit@ukr.net>
2023-12-28 12:49:52 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -5,15 +5,20 @@ registerForm.addEventListener("submit", handleSubmit);
 function handleSubmit(event) {
      event.preventDefault();
     const form = event.target;
-    let email = form.elements.email.value;
-    let password = form.elements.password.value;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
 
     if (email === "" || password === "") {
        return alert('All form fields must be filled in');
     }
-     console.log(`Email: ${email.trim()}, Password: ${password.trim()}`);
+     
+    const userData = {
+      email: email.trim(),
+      password: password.trim()
+    };
+    console.log(userData);
 
-  form.reset();
+   form.reset();
 }
 
 
